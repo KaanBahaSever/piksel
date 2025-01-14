3ca9f4615b629ddbadce23b0aad27d049471719e
Kaan Baha Sever <kaanbahasever@gmail.com>
2025-01-14 10:43:09 +0300

    Fix typo in main.js and expand supported image extensions in renderer.js

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ function createWindow() {
 
     /* mainWindow.webContents.openDevTools() */
 
-    process.platform === "win32" || proccess.platform === "linux" && mainWin.removeMenu();
+    process.platform === "win32" || proccess.platform === "linux" && mainWindow.removeMenu();
     process.platform === "darwin" && Menu.setApplicationMenu(Menu.buildFromTemplate([]))
 
     mainWindow.on('closed', function () {
